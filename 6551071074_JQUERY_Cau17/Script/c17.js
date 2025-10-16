90a7f360372900e1c89c579df4edd37591183e93
quocquyhoccode <dangquocquy2020@gmail.com>
2025-10-16 22:44:11 +0700

    Delete 6551071074_JQUERY_Cau17 directory

--- 6551071074_JQUERY_Cau17/Script/c17.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("h3").click(function() {
-    $(".box").slideUp();
-
-    if (!$(this).next(".box").is(":visible")) {
-      $(this).next(".box").slideDown();
-    }
-  });
-});
