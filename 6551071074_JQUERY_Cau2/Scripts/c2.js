6a0c940b3cab54e8ff327fe314dbdfdbec0d179d
quocquyhoccode <dangquocquy2020@gmail.com>
2025-10-16 22:47:35 +0700

    Delete 6551071074_JQUERY_Cau2 directory

--- 6551071074_JQUERY_Cau2/Scripts/c2.js
+++
@@ -1,6 +0,0 @@
-function js_styles() {
-    var firstname = $("input[name=fname]").val();
-    var lastname = $("input[name=lname]").val();
-    alert("Họ: " + firstname + "\nTên: " + lastname);
-    return false; 
-}
