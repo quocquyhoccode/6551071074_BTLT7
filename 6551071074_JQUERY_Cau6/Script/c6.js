93e57994c2208b6c973148ed3e80b6310dcd6fdd
quocquyhoccode <dangquocquy2020@gmail.com>
2025-10-16 22:48:11 +0700

    Delete 6551071074_JQUERY_Cau6 directory

--- 6551071074_JQUERY_Cau6/Script/c6.js
+++
@@ -1,8 +0,0 @@
-function getOptions(){
-    var count = $("#mySelect option").length;
-    var items = [];
-    $("#mySelect option").each(function(){
-        items.push($(this).text());
-    });
-    alert("Total items: " + count + "\nItems: " + items.join(", "));
-}
